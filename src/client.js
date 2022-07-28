93da8f0dedb3ff5a7f8af0fde5c8a5069ad2f2e3
Lyletimotheus <lyle6494@gmail.com>
2022-07-28 14:21:40 +0200

    sanity linking issues

--- src/client.js
+++ src/client.js
@@ -2,7 +2,7 @@ import sanityClient from '@sanity/client'
 // import imageUrlBuilder from '@sanity/image-url'
 
 export const client = sanityClient({
-    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    projectId: 'qgnoush8',
     dataset: 'production',
     apiVersion: '2022-07-13',
     useCdn: true,
